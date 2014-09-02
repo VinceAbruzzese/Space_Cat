b140620a779263950b33bfa0ea1d60a5e7d3678d
Vince Abruzzese <vinceabruzzese@Vinces-Mac-mini.local>
2014-09-02 16:32:06 -0400

    Initial Commit

---
+++ Space Cat/VMAMyScene.h
@@ -0,0 +1,13 @@
+//
+//  VMAMyScene.h
+//  Space Cat
+//
+
+//  Copyright (c) 2014 Vince Abruzzese. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface VMAMyScene : SKScene
+
+@end
---
+++ Space Cat/VMAViewController.m
@@ -0,0 +1,51 @@
+//
+//  VMAViewController.m
+//  Space Cat
+//
+//  Created by Vince Abruzzese on 2014-09-02.
+//  Copyright (c) 2014 Vince Abruzzese. All rights reserved.
+//
+
+#import "VMAViewController.h"
+#import "VMAMyScene.h"
+
+@implementation VMAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [VMAMyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+@end
